a0d92bc84fdcfb2bf8fe60cebe8d76ec1043632d
authentic <galaxy73rambler@yahoo.com>
2014-09-30 18:32:30 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EndlessHorizons::Application.config.secret_token = 'bdcde5a8412b8032ae559a584965838a45be8fb5be9c06b49c90fb6a09754bfe210b882972c613dd4ddcbf0c1ebf9bdfec22fa167eb23875fe27ca1742570ad6'
